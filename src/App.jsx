98c10c64816fb71d64ea8767ccb6f8e2755ce6d3
Nitiema Allassane <allassanenitiema@gmail.com>
2025-03-10 23:27:12 +0000

    Changer le chemin d'acces a l'image du QR code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 
 // QR code image component
-function Qrcodemage() {
+function QrcodeImage() {
   return(
     <div className="qrimage">
-      <img src="../images/image-qr-code.png" alt="The QR code" />
+      <img src="../public/image-qr-code.png" alt="The QR code" />
     </div>
   )
 }
@@ -21,7 +21,7 @@ function DescriptionOfQrCode({children}) {
 function QrCodeCard() {
   return(
     <div className="codeQrCard">
-        <Qrcodemage />
+        <QrcodeImage />
         <DescriptionOfQrCode>
           <h1>Improve your your front-end skills by building projects</h1>
           <p>
